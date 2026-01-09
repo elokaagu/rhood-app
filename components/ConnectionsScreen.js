b253001afbd21ef8ae8f594578441020e1dc6275
Eloka Agu <eloka@satellitelabs.xyz>
2026-01-09 18:01:45 +0000

    Fix search suggestions z-index to appear above DJ cards
    - Increase searchWrapper z-index from 10 to 1000
    - Add zIndex: 1001 to suggestionsContainer
    - Increase elevation from 8 to 20 for Android
    - Ensures search suggestions dropdown appears in front of all cards

--- components/ConnectionsScreen.js
+++ components/ConnectionsScreen.js
@@ -2686,7 +2686,7 @@ const styles = StyleSheet.create({
   },
   searchWrapper: {
     position: "relative",
-    zIndex: 10,
+    zIndex: 1000,
   },
   searchContainer: {
     flexDirection: "row",
@@ -2727,7 +2727,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 8,
-    elevation: 8,
+    elevation: 20,
+    zIndex: 1001,
   },
   suggestionItem: {
     flexDirection: "row",
